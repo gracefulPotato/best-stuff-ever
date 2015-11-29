3b760927444a742b8bd95a75c27e5859f65cd232
nullP01nterException <jolina.lam@sbcglobal.net>
2015-11-29 02:09:26 -0800

    can now map characters from 1-63 and detects for special characters

--- src/BestStuffEver.java
+++ src/BestStuffEver.java
@@ -1,14 +1,50 @@
+/*Sources:
+ * stackOverflow.com for efficiently finding if string contains special character
+ * */
+
+
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.io.*;
 
 public class BestStuffEver {
 	
-	
 	public static void main(String args[])throws IOException{
 		//Scanner in = new Scanner(new File(args[0]));
-		Scanner in = new Scanner(new File("input/test.txt"));
+		Scanner in = new Scanner(new File("data/test.txt"));
+		Conversion converter = new Conversion();
+		
+		//for finding if string contains special character (stackOverflow.com)
+		Pattern regex = Pattern.compile("[^A-Za-z0-9]");
+	    Matcher match;
+	    
+		int[] bitseq;
+		
 		while(in.hasNextLine()){
-			System.out.println(in.nextLine());
+			String UI = in.nextLine();
+			
+			match = regex.matcher(UI);
+			boolean specialChar = match.find();
+			
+			//incorrect input
+			if(UI.length() > 16){
+				System.out.println(UI + " has more than 16 characters");
+			}
+			else if(specialChar){
+				System.out.println(UI + " can only contain 0-9, a-z, or A-Z");
+			}
+			//correct input
+			else{
+				bitseq = converter.stringToBitseq(UI);
+				System.out.print(UI + ": ");
+				for(int i = 0; i < bitseq.length; i++){
+					System.out.print(bitseq[i] + " ");
+				}
+				System.out.println();
+			}
+			
+			
 		}
 		in.close();
 	}
@@ -21,5 +57,6 @@ public class BestStuffEver {
 			else System.out.println("KIIRO TO ITTARA BANANA");
 			
 		}
+		System.out.print("");
 	}
 }
--- src/Conversion.java
+++ src/Conversion.java
@@ -4,17 +4,28 @@ public class Conversion {
 	//representing each char in s by its assigned
 	//bit pattern
 	public Conversion(){}
+	
 	int[] stringToBitseq(String s){
-		int[] arr = new int[1];
+		String characters = "0123456789abcdefghijklmnopqurstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		char[] charArray = s.toCharArray();
+		int[] arr = new int[charArray.length];
+		
+		for(int i = 0; i < charArray.length; i++){
+			arr[i] = characters.indexOf(charArray[i]) + 1;
+		}
+		
 		return arr;
 	}
+	
 	int[] bitseqToDigitSeq(){
 		int[] arr = new int[1];
 		return arr;
 	}
+	
 	long bitSeqToBigNum(){
 		return 0;
 	}
+	
 	int[] numToBitSeq(int n){
 		int[] arr = new int[1];
 		return arr;
